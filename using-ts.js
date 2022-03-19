4dd339b480f245a232c9ecd58f01c866835960fe
Linh-coder <Linhtran2391999@gmail.com>
2022-03-19 16:10:41 +0700

    learn data type_v1

--- using-ts.js
+++ using-ts.js
@@ -1,10 +1,41 @@
 var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
-
+var n1 = '5';
+// enum
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+// define object type
+var person1 = {
+    name: 'Max',
+    age: 30
+};
+// or define
+var person2 = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['Sport', 'Cooking'],
+    role: Role.ADMIN
+};
+// define array
+var favoriteActivities;
+console.log(person1.name);
+for (var _i = 0, _a = person2.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
+if (person2.role === Role.ADMIN) {
+    console.log("is Admin");
+}
+// In function, we can define data type of varient 
 function add(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
--- using-ts.ts
+++ using-ts.ts
@@ -1,7 +1,43 @@
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
+let n1 = '5';
 
+// enum
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+// define object type
+const person1:{
+    name: string;
+    age: number;
+} = {
+    name: 'Max',
+    age: 30
+};
+
+// or define
+const person2 ={
+    name: 'Max',
+    age: 30,
+    hobbies: ['Sport', 'Cooking'],
+    role: Role.ADMIN
+};
+
+// define array
+let favoriteActivities: any[];
+
+console.log(person1.name);
+for (const hobby of person2.hobbies) {
+    console.log(hobby);
+}
+
+
+if (person2.role === Role.ADMIN) {
+    console.log("is Admin");
+    
+}
+
+// In function, we can define data type of varient 
 function add(num1: number, num2: number)
 {
     return num1 + num2;
